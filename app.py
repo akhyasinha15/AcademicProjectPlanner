2b5588162a804f0d84315945c506274250c8c4ba
Akhya Sinha <kaoriylia0@gmail.com>
2026-03-30 22:19:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import Config
+from data.db import db
+from flask_cors import CORS
+from presentation.routes import main_routes
+from presentation.auth_routes import auth_routes
+
+app = Flask(__name__)
+CORS(app)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+app.register_blueprint(main_routes)
+app.register_blueprint(auth_routes)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
